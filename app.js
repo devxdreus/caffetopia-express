71e6c218ba67322c35b4bb9b124c49a6ebda7616
devxdreus <andricrafter007@gmail.com>
2024-07-17 09:32:35 +0800

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 app.use(
     cors({
-        origin: ['*'],
+        origin: ['https://coffetopia.my.id'],
         methods: ['POST', 'GET', 'PUT', 'DELETE'],
         credentials: true,
     })
