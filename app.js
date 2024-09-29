df245302b2fd55f7cd347dd198a2c2e108fe9a4f
devxdreus <andricrafter007@gmail.com>
2024-09-29 15:31:04 +0800

    disable cors origin

--- app.js
+++ app.js
@@ -8,18 +8,12 @@ const routes = require('./routes');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 
-app.use(
-    cors({
-        origin: ['https://coffetopia.my.id/'],
-        methods: ['POST', 'GET', 'PUT', 'DELETE'],
-        credentials: true,
-    })
-);
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use('/api', routes);
 app.use('/api/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.listen(process.env.PORT, () => {
-    console.log(`App listen on http://localhost:${process.env.PORT}`);
+  console.log(`App listen on http://localhost:${process.env.PORT}`);
 });
